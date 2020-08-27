0e994335d3e872a1c314f5a28eb5168e4d475d7f
HJKim95 <25ephipany@naver.com>
2020-08-27 10:26:05 +0900

    initial commits

---
+++ Example/Pods/Target Support Files/HJWeather/HJWeather-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HJWeather : NSObject
+@end
+@implementation PodsDummy_HJWeather
+@end
---
+++ Example/Pods/Target Support Files/Pods-HJWeather_Example/Pods-HJWeather_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HJWeather_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HJWeather_Example
+@end
